c7f58ed9facfabb1ccda54106cee6c45cd717adb
Gabriel Mendes <luizgabrielpm@gmail.com>
2024-06-13 14:53:18 -0300

    feat(ContaBancaria): create new ContaBancaria and saque operation

--- src/main/java/com/algaworks/junit/utilidade/ContaBancaria.java
+++ src/main/java/com/algaworks/junit/utilidade/ContaBancaria.java
@@ -4,15 +4,29 @@ import java.math.BigDecimal;
 
 public class ContaBancaria {
 
+    private BigDecimal saldo;
+
     public ContaBancaria(BigDecimal saldo) {
         //TODO 1 - validar saldo: não pode ser nulo, caso seja, deve lançar uma IllegalArgumentException
+        if (saldo == null)
+            throw new IllegalArgumentException("O saldo não pode ser nulo.");
         //TODO 2 - pode ser zero ou negativo
     }
 
     public void saque(BigDecimal valor) {
         //TODO 1 - validar valor: não pode ser nulo, zero ou menor que zero, caso seja, deve lançar uma IllegalArgumentException
+        if (valor == null || valor.compareTo(BigDecimal.ZERO) > 0)
+            throw new IllegalArgumentException("O saldo não pode ser nulo, zero ou menor que zero.");
+        
         //TODO 2 - Deve subtrair o valor do saldo
+        BigDecimal saldoAfterSaque = saldo.subtract(valor);
+
         //TODO 3 - Se o saldo for insuficiente deve lançar uma RuntimeException
+        if(saldoAfterSaque.compareTo(BigDecimal.ZERO) < 0)
+            throw new RuntimeException("Saldo insuficiente.");
+
+        this.saldo = saldoAfterSaque;
+
     }
 
     public void deposito(BigDecimal valor) {
