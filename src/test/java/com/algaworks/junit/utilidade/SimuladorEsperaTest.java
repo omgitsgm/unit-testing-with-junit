a277f22c736aad68e9aad0dca79addf5957f7db7
Gabriel Mendes <luizgabrielpm@gmail.com>
2024-06-13 13:58:03 -0300

    style: removed unused imports in SimuladorEsperaTest

--- src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
@@ -6,7 +6,6 @@ import java.time.Duration;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
 class SimuladorEsperaTest {
 
