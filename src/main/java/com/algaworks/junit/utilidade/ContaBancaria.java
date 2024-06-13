c9121ff036c11cc29fd4b10763f419e4e0dc9f35
Gabriel Mendes <luizgabrielpm@gmail.com>
2024-06-13 16:24:23 -0300

    feat(ContaBancaria): deposito, saldo and aux method.
    aux method: isValidValorOrElseThrowException - check if a 'valor' is null, zero or less than zero and throws an IllegalArgumentException.

--- src/main/java/com/algaworks/junit/utilidade/ContaBancaria.java
+++ src/main/java/com/algaworks/junit/utilidade/ContaBancaria.java
@@ -10,14 +10,15 @@ public class ContaBancaria {
         //TODO 1 - validar saldo: não pode ser nulo, caso seja, deve lançar uma IllegalArgumentException
         if (saldo == null)
             throw new IllegalArgumentException("O saldo não pode ser nulo.");
+
         //TODO 2 - pode ser zero ou negativo
         this.saldo = saldo;
+
     }
 
     public void saque(BigDecimal valor) {
         //TODO 1 - validar valor: não pode ser nulo, zero ou menor que zero, caso seja, deve lançar uma IllegalArgumentException
-        if (valor == null || valor.compareTo(BigDecimal.ZERO) <= 0)
-            throw new IllegalArgumentException("O saldo não pode ser nulo, zero ou menor que zero.");
+        isValidValorOrElseThrowException(valor);
         
         //TODO 2 - Deve subtrair o valor do saldo
         BigDecimal saldoAfterSaque = saldo.subtract(valor);
@@ -32,12 +33,23 @@ public class ContaBancaria {
 
     public void deposito(BigDecimal valor) {
         //TODO 1 - validar valor: não pode ser nulo, zero ou menor que zero, caso seja, deve lançar uma IllegalArgumentException
+        isValidValorOrElseThrowException(valor);
+
         //TODO 2 - Deve adicionar o valor ao saldo
+        this.saldo = saldo.add(valor);
+
     }
 
     public BigDecimal saldo() {
         //TODO 1 - retornar saldo
-        return null;
+        return this.saldo;
+    }
+
+    private static void isValidValorOrElseThrowException(BigDecimal valor) {
+        //TODO 1 - validar valor: não pode ser nulo, zero ou menor que zero, caso seja, deve lançar uma IllegalArgumentException
+        if (valor == null || valor.compareTo(BigDecimal.ZERO) <= 0)
+            throw new IllegalArgumentException("O valor não pode ser nulo, zero ou menor que zero.");
+
     }
 
     public BigDecimal getSaldo() {
