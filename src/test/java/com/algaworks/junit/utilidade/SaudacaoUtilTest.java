7b6e8f772dc5c00bca720d80c1d964bd96d53e3b
Gabriel Mendes <luizgabrielpm@gmail.com>
2024-06-14 12:27:57 -0300

    refactor(SaudacaoUtilTest): nomenclatura BDD

--- src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -11,9 +11,11 @@ import org.junit.jupiter.api.function.Executable;
 @DisplayName("Testes no utilitário de saudação.")
 class SaudacaoUtilTest {
     
+    // BDD    : givenAnHourBetween0and11_whenSaudar_thenReturnBomDia
+    // Default: saudar_withHoraBetween0and11_returnsBomDia
     @Test
-    @DisplayName("Deve saudar com bom dia.")
-    void saudar_withHoraBetween0and11_returnsBomDia() {
+    @DisplayName("Deve saudar com bom dia em um horário entre 0h e 11h.")
+    void givenAnHourBetween0and11_whenSaudar_thenReturnBomDia() {
         // Arrange
         int horaBetween0and11 = 9;
         String expectedSaudacao = "Bom dia";
@@ -25,9 +27,11 @@ class SaudacaoUtilTest {
         assertEquals(expectedSaudacao, actualSaudacao, "Saudação incorreta!");
     }
 
+    // BDD    : givenAnHourEqualTo5_whenSaudar_thenReturnBomDia
+    // Default: saudar_withHora5_returnsBomDia
     @Test
     @DisplayName("Deve saudar com bom dia às 5h.")
-    void saudar_withHora5_returnsBomDia() {
+    void givenAnHourEqualTo5_whenSaudar_thenReturnBomDia() {
 
         int hora5 = 5;
         String expectedSaudacao = "Bom dia";
@@ -37,9 +41,11 @@ class SaudacaoUtilTest {
         assertEquals(expectedSaudacao, saudacao, "Saudação incorreta!");
     }
 
+    // BDD    : givenAnHourBetween12and17_whenSaudar_thenReturnBoaTarde
+    // Default: saudar_withHoraBetween12and17_returnsBoaTarde
     @Test
-    @DisplayName("Deve saudar com boa tarde.")
-    void saudar_withHoraBetween12and17_returnsBoaTarde() {
+    @DisplayName("Deve saudar com boa tarde em um horário entre 12h e 17h.")
+    void givenAnHourBetween12and17_whenSaudar_thenReturnBoaTarde() {
 
         int horaBetween12and17 = 15;
         String expectedSaudacao = "Boa tarde";
@@ -49,9 +55,11 @@ class SaudacaoUtilTest {
         assertEquals(expectedSaudacao, saudacao, "Saudação incorreta!");
     }
 
+    // BDD    : givenAnHourBetween18and23_whenSaudar_thenReturnBoaNoite
+    // Default: saudar_withHoraBetween18and23_returnsBoaNoite
     @Test
-    @DisplayName("Deve saudar com boa noite, entre 18h e 23h.")
-    void saudar_withHoraBetween18and23_returnsBoaNoite() {
+    @DisplayName("Deve saudar com boa noite entre 18h e 23h.")
+    void givenAnHourBetween18and23_whenSaudar_thenReturnBoaNoite() {
 
         int horaBetween18and23 = 21;
         String expectedSaudacao = "Boa noite";
@@ -61,9 +69,11 @@ class SaudacaoUtilTest {
         assertEquals(expectedSaudacao, saudacao, "Saudação incorreta!");
     }
 
+    // BDD    : givenAnHourEqualTo4_whenSaudar_thenReturnBoaNoite
+    // Default: saudar_withHora4_returnsBoaNoite
     @Test
     @DisplayName("Deve saudar com boa noite às 4h.")
-    void saudar_withHora4_returnsBoaNoite() {
+    void givenAnHourEqualTo4_whenSaudar_thenReturnBoaNoite() {
 
         int hora4 = 4;
         String expectedSaudacao = "Boa noite";
@@ -73,9 +83,11 @@ class SaudacaoUtilTest {
         assertEquals(expectedSaudacao, saudacao, "Saudação incorreta!");
     }
 
+    // BDD    : givenAnInvalidHour_whenSaudar_thenThrowAnIllegalArgumentException
+    // Default: saudar_withInvalidHora_throwsIllegalArgumentException
     @Test
-    @DisplayName("Deve lançar IllegalArgumentException com hora inválida.")
-    void saudar_withInvalidHora_throwsIllegalArgumentException() {
+    @DisplayName("Deve lançar IllegalArgumentException quando saudar com hora inválida.")
+    void givenAnInvalidHour_whenSaudar_thenThrowAnIllegalArgumentException() {
 
         // Arrange
         int invalidHora = -10;
@@ -89,9 +101,11 @@ class SaudacaoUtilTest {
         assertEquals(expectedExceptionMessage, illegalArgumentException.getMessage());
     }
 
+    // BDD    : givenAValidHour_whenSaudar_thenDoNotThrowAnyException
+    // Default: saudar_withValidHora_doesNotThrowException
     @Test
-    @DisplayName("Não deve lançar exceção com hora válida.")
-    void saudar_withValidHora_doesNotThrowException() {
+    @DisplayName("Não deve lançar exceção quando saudar com hora válida.")
+    void givenAValidHour_whenSaudar_thenDoNotThrowAnyException() {
 
         int validHora = 0;
 
