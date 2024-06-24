0ae04d358b7e0ae7a7ecb67add56df0495571956
Gabriel Mendes <luizgabrielpm@gmail.com>
2024-06-24 19:37:35 -0300

    test (CarrinhoCompraTest): testes no método de adicionar produto ao carrinho

---
+++ src/test/java/com/algaworks/junit/ecommerce/CarrinhoCompraTest.java
@@ -0,0 +1,185 @@
+package com.algaworks.junit.ecommerce;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+@DisplayName("Testes em CarrinhoCompra")
+class CarrinhoCompraTest {
+
+    static Cliente cliente;
+    static Produto produto;
+    static ItemCarrinhoCompra item;
+    static CarrinhoCompra carrinho;
+
+    static String parametrosNaoPodemSerNullMessage = "Parâmetros não podem ser null.";
+    static String quantidadeMenorDoQueUmMessage = "Quantidade não pode ser menor do que 1.";
+
+
+    @BeforeAll
+    static void beforeAll() {
+
+        cliente = new Cliente(1L, "Gabriel");
+        produto = new Produto(1L, "Playstation 5", "O novo console da família Playstation.", new BigDecimal(3700));
+        item = new ItemCarrinhoCompra(produto, 1);
+
+    }
+
+
+    @BeforeEach
+    void beforeEach() {
+
+        carrinho = new CarrinhoCompra(cliente, List.of(item));
+
+    }
+
+
+    @DisplayName("Dada uma instância de carrinho de compra.")
+    @Nested
+    class GivenAnInstanceOfCarrinhoCompra {
+
+        @DisplayName("quando pegar itens.")
+        @Nested
+        class WhenGetItens {
+            
+            @DisplayName("retorne os itens do carrinho.")
+            @Test
+            void thenReturnItensFromCarrinho() {
+                
+                List<ItemCarrinhoCompra> expectedListOfItens = List.of(item);
+
+                List<ItemCarrinhoCompra> actualItens = carrinho.getItens();
+
+                assertIterableEquals(expectedListOfItens, actualItens);
+
+            }
+            
+        }
+
+        @DisplayName("quando adicionar produto ao carrinho.")
+        @Nested
+        class whenAdicionarProduto {
+
+            @DisplayName("com produto null.")
+            @Nested
+            class withProdutoNull {
+
+                @DisplayName("então lance uma NullPointerException.")
+                @Test
+                void thenThrowNullPointerException() {
+
+                    Executable executable = () -> carrinho.adicionarProduto(null, 1);
+
+                    NullPointerException exception = assertThrows(NullPointerException.class, executable);
+
+                    assertEquals(parametrosNaoPodemSerNullMessage, exception.getMessage());
+
+                }
+
+            }
+            
+            @DisplayName("com quantidade null.")
+            @Nested
+            class withQuantidadeNull {
+
+                @DisplayName("então lance uma NullPointerException.")
+                @Test
+                void thenThrowNullPointerException() {
+
+                    Executable executable = () -> carrinho.adicionarProduto(produto, null);
+
+                    NullPointerException exception = assertThrows(NullPointerException.class, executable);
+
+                    assertEquals(parametrosNaoPodemSerNullMessage, exception.getMessage());
+
+                }
+
+            }
+
+            @DisplayName("com quantidade menor do que 1.")
+            @Nested
+            class withQuantidadeLessThanOne {
+
+                @DisplayName("então lance uma IllegalArgumentException.")
+                @Test
+                void thenThrowIllegalArgumentException() {
+
+                    Executable executable = () -> carrinho.adicionarProduto(produto, 0);
+
+                    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, executable);
+
+                    assertEquals(quantidadeMenorDoQueUmMessage, exception.getMessage());
+
+                }
+
+            }
+
+            @DisplayName("com produto que já está no carrinho.")
+            @Nested
+            class withProdutoJaExistente {
+
+                @DisplayName("então incremente a quantidade do item no carrinho.")
+                @Test
+                void thenIncrementaQuantidade() {
+
+                    int expectedQuantidade = 2;
+
+                    carrinho.adicionarProduto(produto, 1);
+
+                    assertEquals(expectedQuantidade, carrinho.getItens().get(0).getQuantidade());
+
+                }
+
+            }
+
+            @DisplayName("com produto novo.")
+            @Nested
+            class withNovoProduto {
+
+                static Produto novoProduto;
+
+                @BeforeAll
+                static void beforeAll() {
+                    novoProduto = new Produto(7L, "Novo Produto", "Descrição do novo produto", new BigDecimal(1));
+                }
+
+                @DisplayName("então adicione o novo produto ao carrinho.")
+                @Test
+                void thenAdicioneNovoProduto() {
+
+                    carrinho.adicionarProduto(novoProduto, 1);
+
+                    assertEquals(novoProduto, carrinho.getItens().get(1).getProduto());
+
+                }
+
+                @DisplayName("então incremente o tamanho da lista de itens do carrinho.")
+                @Test
+                void thenUpdateListSize() {
+
+                    int expectedSize = 2;
+
+                    carrinho.adicionarProduto(novoProduto, 1);
+
+                    assertEquals(expectedSize, carrinho.getItens().size());
+
+                }
+
+            }
+
+        }
+
+    }
+
+    
+}
